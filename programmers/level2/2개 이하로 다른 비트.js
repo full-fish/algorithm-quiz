708be0faf18c3aa0ae070b6a088a4ff762362676
manseon <91870110+full-fish@users.noreply.github.com>
2023-02-22 23:06:05 +0900

    feat

--- programmers/level2/2개 이하로 다른 비트.js
+++ programmers/level2/2개 이하로 다른 비트.js
@@ -1,6 +1,25 @@
 function solution(numbers) {
+  let result = []
   numbers.forEach(ele => {
     let binary = ele.toString(2)
+    let next = ele + 1
+    while (1) {
+      let nextBinary = next.toString(2)
+      if (differentBit(binary, nextBinary)) return result.push(next)
+      else next++
+    }
   })
+  return result
 }
-console.log((3).toString(2))
+function differentBit(a, b) {
+  let result = 0
+  const long = Math.max(a.length, b.length)
+  a = a.padStart(long, '0')
+  b = b.padStart(long, '0')
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) result++
+    else if (result > 2) return false
+  }
+  return result === 1 || result === 2 ? true : false
+}
+console.log(solution([2, 7]))
