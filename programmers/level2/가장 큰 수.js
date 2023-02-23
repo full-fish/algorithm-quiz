b45c2485b569aed3121d49f8d2f2393249eba833
full-fish <manseon94@gmail.com>
2023-02-23 16:46:36 +0900

    feat

--- programmers/level2/가장 큰 수.js
+++ programmers/level2/가장 큰 수.js
@@ -1,6 +1,10 @@
 let numbers = [3, 31, 34, 5, 9]
 function solution(numbers) {
-  console.log(numbers.sort())
-  console.log([6, 10, 2].sort())
+  let result = numbers
+    .map(ele => String(ele))
+    .sort((a, b) => (a + b > b + a ? -1 : a + b < b + a ? 1 : 0))
+    .join('')
+  if (/^0+$/.test(result)) return '0'
+  return result
 }
 console.log(solution(numbers))
