5642aad8ccce682bef9cfc8feafa70663afe2ff6
manseon <91870110+full-fish@users.noreply.github.com>
2023-02-28 23:24:33 +0900

    feat

--- programmers/level3/이중우선순위큐.js
+++ programmers/level3/이중우선순위큐.js
@@ -1,31 +1,39 @@
-let operations = ['I 16', 'I -5643', 'D -1', 'D 1', 'D 1', 'I 123', 'D -1']
+let operations = ['I -45', 'I 653', 'D 1', 'I -642', 'I 45', 'I 97', 'D 1', 'D -1', 'I 333']
 function solution(operations) {
   let q = []
   let maxNum = []
   let minNum = []
   let maxIndex = [0]
   let minIndex = [0]
+  //   operations.forEach(ele => {
+  //     let [, a, n] = [...ele.match(/([A-Z]) (-*\d+)/)]
+  //     n = Number(n)
+  //     if (a === 'I') {
+  //       q.push(n)
+  //       if (n > maxNum[0]) {
+  //         maxNum.unshift(n)
+  //         maxIndex.unshift(q.length - 1)
+  //       } else if (n < minNum[0]) {
+  //         minNum.unshift(n)
+  //         minIndex.unshift(q.length - 1)
+  //       }
+  //     } else if (n === 1) {
+  //       q.splice(maxIndex.shift(), 1)
+  //       maxNum.shift()
+  //     } else {
+  //       q.splice(minIndex.shift(), 1)
+  //       minNum.shift()
+  //     }
+  //   })
   operations.forEach(ele => {
     let [, a, n] = [...ele.match(/([A-Z]) (-*\d+)/)]
     n = Number(n)
     if (a === 'I') {
       q.push(n)
-      if (n > maxNum[0]) {
-        maxNum.unshift(n)
-        maxIndex.unshift(q.length - 1)
-      } else if (n < minNum[0]) {
-        minNum.unshift(n)
-        minIndex.unshift(q.length - 1)
-      }
-    } else if (n === 1) {
-      q.splice(maxIndex.shift(), 1)
-      maxNum.shift()
-    } else {
-      q.splice(minIndex.shift(), 1)
-      minNum.shift()
-    }
+    } else if (n === 1) q.splice(q.indexOf(Math.max(...q)), 1)
+    else q.splice(q.indexOf(Math.min(...q)), 1)
   })
-  return q.length ? [maxNum[0], minNum[0]] : [0, 0]
+  return q.length ? [Math.max(...q), Math.min(...q)] : [0, 0]
 }
 
 console.log(solution(operations))
