956dd883103e45cce109ef8abdb1c49e8a8fbdb3
full-fish <manseon94@gmail.com>
2023-03-20 09:36:35 +0900

    feat

--- programmers/level2/124 나라의 숫자.js
+++ programmers/level2/124 나라의 숫자.js
@@ -1,10 +1,17 @@
 function solution(n) {
   let str = ''
-  while (n > 3) {
-    str += n % 3
+  while (n >= 3) {
+    let remainder = n % 3
     n = parseInt(n / 3)
+    if (remainder === 0) {
+      str = 4 + str
+      n -= 1
+    } else str = remainder + str
   }
   n += str
-  return n.replace(/3/, '4')
+  return n.replace(/^0*/, '')
 }
-console.log(solution(6))
+//! 다른 사람 코드
+function change124(n) {
+  return n === 0 ? '' : change124(parseInt((n - 1) / 3)) + [1, 2, 4][(n - 1) % 3]
+}
