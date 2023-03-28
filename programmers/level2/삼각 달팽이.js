75b9180a6baed7cd9fd66003d3d34ad4b4da2d3a
full-fish <manseon94@gmail.com>
2023-03-28 14:25:49 +0900

    feat

--- programmers/level2/삼각 달팽이.js
+++ programmers/level2/삼각 달팽이.js
@@ -1,3 +1,4 @@
+//! 처음에 못푼거
 function solution(n) {
   let arr = [...Array(n).keys()].map(ele => new Array(ele + 1).fill(0))
   let num = 1
@@ -48,3 +49,61 @@ function solution(n) {
   console.log(arr)
 }
 console.log(solution(6))
+
+//! 푼거
+function solution2(n) {
+  if (n === 1) return [1]
+  let arr = Array.from({ length: n }, () => Array.from({ length: 0 }))
+  let num = 1
+  let hP = 0
+  let vP = 0
+  while (num <= (n * (n + 1)) / 2) {
+    while (vP < arr.length - hP - 1 && arr[vP].length < vP + 1) {
+      const cur = arr[vP]
+      if (hP === 0) arr[vP].push(num)
+      else arr[vP] = cur.slice(0, hP).concat(num).concat(cur.slice(hP))
+      num++
+      vP++
+    }
+    let temp = []
+    while (arr[vP].length + temp.length < vP + 1) {
+      temp.push(num)
+      num++
+    }
+    arr[vP] = arr[vP].slice(0, hP).concat(temp).concat(arr[vP].slice(hP))
+    vP--
+    while (arr[vP].length < vP + 1) {
+      const cur = arr[vP]
+      if (hP === 0) arr[vP].push(num)
+      else
+        arr[vP] = cur
+          .slice(0, cur.length - hP)
+          .concat(num)
+          .concat(cur.slice(-hP))
+      vP--
+      num++
+    }
+    vP++
+    vP++
+    hP++
+  }
+  return arr.flat()
+}
+
+//! 다른사람 코드
+function solution2(n) {
+  let a = Array(n)
+    .fill()
+    .map((_, i) => Array(i + 1).fill())
+  console.log(a)
+  let row = -1
+  let col = 0
+  let fill = 0
+  for (let i = n; i > 0; i -= 3) {
+    a[++row][col] = ++fill
+    for (let j = 0; j < i - 1; j++) a[++row][col] = ++fill
+    for (let j = 0; j < i - 1; j++) a[row][++col] = ++fill
+    for (let j = 0; j < i - 2; j++) a[--row][--col] = ++fill
+  }
+  return a.flat()
+}
