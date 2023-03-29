32bc894eafaa14f70f5bbfee700b460f0321788f
full-fish <manseon94@gmail.com>
2023-03-29 16:26:41 +0900

    feat

--- programmers/level2/숫자 변환하기.js
+++ programmers/level2/숫자 변환하기.js
@@ -13,3 +13,23 @@ function solution(x, y, n) {
   DFS(0, x)
   return result.length ? Math.min(...result) : -1
 }
+//! 개선
+function solution(x, y, n) {
+  let min = Number.MAX_SAFE_INTEGER
+  let obj = {}
+  function DFS(count, sum) {
+    if (!Number.isInteger(sum)) return -1
+    if (!(sum in obj) || (sum in obj && obj[sum] > count)) obj[sum] = count
+    if (sum in obj && obj[sum] < count) return -1
+    if (sum === x && min > count) min = count
+    else if (sum < x) {
+      return -1
+    } else {
+      DFS(count + 1, sum / 3)
+      DFS(count + 1, sum / 2)
+      DFS(count + 1, sum - n)
+    }
+  }
+  DFS(0, y)
+  return min === Number.MAX_SAFE_INTEGER ? -1 : min
+}
