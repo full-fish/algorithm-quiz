73b7a83f40459939efd6a9af42e310c810bb9286
manseon <91870110+full-fish@users.noreply.github.com>
2023-03-31 23:46:06 +0900

    feat

--- programmers/level2/dnqkr.js
+++ programmers/level2/dnqkr.js
@@ -8,7 +8,6 @@ let ranges = [
 function solution(k, ranges) {
   let arr = []
   let x = 0
-  let sum = 0
   let gArr = []
   while (k !== 1) {
     arr.push([x, k])
@@ -16,14 +15,17 @@ function solution(k, ranges) {
     x++
   }
   arr.push([x, k])
-  for (let i = 0; i < arr.length - 1; i++) {
-    const a = (arr[i + 1][1] - arr[i][1]) / (arr[i + 1][0] - arr[i][0])
-    const c = -(arr[i][0] * a) + arr[i][1]
-    let tt = (a / 2) * arr[i + 1][0] ** 2 + c * arr[i + 1][0] - ((a / 2) * arr[i][0] ** 2 + c * arr[i][0])
-    sum += tt
-    console.log('tt', tt)
+  for (let k = 0; k < ranges.length; k++) {
+    const [s, e] = ranges[k]
+    let sum = 0
+    for (let i = 0 + s; i < arr.length - 1 + e; i++) {
+      const a = (arr[i + 1][1] - arr[i][1]) / (arr[i + 1][0] - arr[i][0])
+      const c = -(arr[i][0] * a) + arr[i][1]
+      let part = (a / 2) * arr[i + 1][0] ** 2 + c * arr[i + 1][0] - ((a / 2) * arr[i][0] ** 2 + c * arr[i][0])
+      sum += part
+    }
+    if (s > arr.length - 1 + e) sum = -1
+    gArr.push(sum)
   }
-  console.log(sum)
-  return arr
+  return gArr
 }
-console.log(solution(k, ranges))
