e0c031854fcbe9e4b713c11a9c54cbd1ad6b7a64
manseon <91870110+full-fish@users.noreply.github.com>
2025-09-30 15:32:48 +0900

    f

--- programmers/level2/주식가격.py
+++ programmers/level2/주식가격.py
@@ -1,14 +1,82 @@
 prices = [1, 2, 3, 2, 3]
 
-prices2 = [1, 3, 5, 7, 5, 3, 5, 3]  # [7, 6, 3, 1, 1, 2, 1, 0]
+# prices2 = [1, 3, 5, 7, 5, 3, 5, 3]  # [7, 6, 3, 1, 1, 2, 1, 0]
+
+
+# def solution(prices):
+#     result_arr = [0] * len(prices)
+#     index_arr = []
+#     length = len(prices)
+#     for index, price in enumerate(prices):
+#         index_arr.append(index)
+#         if index == 0:
+#             continue
+#         for e, value in enumerate(index_arr):
+#             print(
+#                 "index_arr", index_arr, "value", value, "result_arr", result_arr, price
+#             )
+#             if e == len(index_arr) - 1:
+#                 break
+#             if price >= prices[value]:
+#                 result_arr[value] += 1
+#             else:
+#                 index_arr.pop(value)
+#     return result_arr
+
+
+# print(solution(prices))
 
 
 def solution(prices):
-    stack = []
-    for price in prices:
-        print("1")
-    return 0
+    n = len(prices)
+    result = [0] * n
+    stack = []  # 아직 기간이 확정되지 않은 인덱스들
 
+    for i, price in enumerate(prices):
+        # 가격이 떨어졌을 때 이전 가격들의 기간을 확정
+        while stack and prices[stack[-1]] > price:
+            j = stack.pop()
+            result[j] = i - j
+        stack.append(i)
+
+    # 끝까지 안 떨어진 경우 처리
+    while stack:
+        j = stack.pop()
+        result[j] = n - 1 - j
+
+    return result
+
+
+"""''
+stack [0] 스텍
+
+price 2
+stack [0,1]
+
+price 3
+stack [0,1,2]
+
+price 2
+j = 2
+result [0,0,1,0,0]
+stack [0.1.3]
+
+price 3
+stack [0,1,3,4]
+
+j = 4
+result [0,0,1,0,0]
+j = 3
+result [0,0,1,1,0]
+j = 1
+result [0,3,1,0,0]
+j = 0
+result [4,3,1,0,0]  
+             
+
+
+
+""" ""
 
 """''
 [1,2,3,2,3]
@@ -18,9 +86,21 @@ def solution(prices):
 [1,2,3]
 [1,2,3]
 
+[1, 2, 3, 2, 3]
 now = 1
+index arr = [0]
 [0]
 
 now = 2
+index arr = [0,1]
 [1, 0]
 """ ""
+
+
+# from collections import deque
+
+# arr = [1, 2, 3, 4, 5]
+# dq = deque(arr)
+# print(dq.appendleft(10))
+# print(dq)
+# print(list(dq))
---
+++ programmers/level3/[카카오 인턴] 보석 쇼핑.py
@@ -0,0 +1,93 @@
+gems = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+gems2 = ["AA", "AB", "AC", "AA", "AC"]
+gems3 = ["XYZ", "XYZ", "XYZ"]
+gems4 = ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
+
+
+# def solution(gems):
+#     gems_length = len(gems)
+#     # 객체 생성
+#     origin_obj = {}
+#     for gem in gems:
+#         if not (gem in origin_obj):
+#             origin_obj[gem] = True
+#     origin_obj_key_length = len(origin_obj)
+
+#     result_obj = {}
+#     result_range = [0, float("inf")]
+#     pointer_1 = 0
+#     pointer_2 = 0
+
+#     while True:
+#         result_obj_key_length = len(result_obj)
+#         if pointer_2 > gems_length - 1:
+#             break
+#         # 모든 보석
+
+#         if origin_obj_key_length == result_obj_key_length:
+
+#             # 더 범위가 짧다면
+#             if result_range[1] - result_range[0] > pointer_2 - pointer_1:
+#                 result_range = [pointer_1, pointer_2 - 1]
+#             temp_key = gems[pointer_1]
+#             result_obj[temp_key] -= 1
+#             if result_obj[temp_key] == 0:
+#                 del result_obj[temp_key]
+#             pointer_1 += 1
+
+#         else:
+#             if not (gems[pointer_2] in result_obj):
+#                 result_obj[gems[pointer_2]] = 1
+#             else:
+#                 result_obj[gems[pointer_2]] += 1
+#             pointer_2 += 1
+#         print(
+#             "pointer_1",
+#             pointer_1,
+#             "pointer_2",
+#             pointer_2,
+#             origin_obj_key_length,
+#             result_obj_key_length,
+#         )
+#     return list(map(lambda x: x + 1, result_range))
+
+
+def solution(gems):
+    gems_len = len(gems)
+    # 객체 생성
+    gem_kind_len = len(set(gems))
+
+    result_obj = {}
+    result_range = (0, float("inf"))
+    left = 0
+    right = 0
+
+    while True:
+        # 모든 보석
+        if gem_kind_len == len(result_obj):
+            # 더 범위가 짧다면
+            if result_range[1] - result_range[0] > right - left:
+                result_range = (left, right)
+            temp_key = gems[left]
+            result_obj[temp_key] -= 1
+            if result_obj[temp_key] == 0:
+                del result_obj[temp_key]
+            left += 1
+            # rgith는 이미 오른쪽 최대에서 1칸 넘어간 상태 && 모든 보석 아님
+        elif right == gems_len:
+            break
+        else:
+            temp_key = gems[right]
+            result_obj[temp_key] = result_obj.get(temp_key, 0) + 1
+            right += 1
+    return [result_range[0] + 1, result_range[1]]
+
+
+print(solution(gems))
+print(solution(gems2))
+print(solution(gems3))
+print(solution(gems4))
+"""''
+모든 보석을 모은 상태라면 더 줄일 수 있는지 확인 위해 point1 --
+모든 보석을 모으지 않은 상태라면 point2 ++
+"""
